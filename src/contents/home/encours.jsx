eab264c2a795d474a25012f06d53259fd9651ca1
yanndav <yann.collindavid@gmail.com>
2026-07-08 16:28:42 +0200

    format data

---
+++ src/contents/home/encours.jsx
@@ -0,0 +1,16 @@
+export const encours = [
+  {
+    title: "Insomnies rurales",
+    type: "photo - son - écriture",
+    description:
+      "Dans ce projet en cours, je m’interroge sur les rapports à la nuit des habitants d’un village périurbain qui a éteint les lumières publiques.",
+    photo: "P1000393.jpg",
+  },
+  {
+    title: "Partager la nuit",
+    type: "vidéo",
+    description:
+      "Voilà quinze ans que Pérignat-ès-Allier, petit village auvergnat de mille cinq cent habitants, a décidé d’éteindre l’éclairage public nocturne afin de préserver l’obscurité. La compagnie Luüm arrive de Paris pour créer un spectacle indédit de déambulation de nuit.",
+    photo: "P1010764.jpeg",
+  },
+];
